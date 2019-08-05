e7fa679e9bfe56bc2b707b49c4b0480f79f400ce
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2019-08-06 00:31:14 +0900

    Update 17140_array_calc_unsolve.cpp

--- 17140_array_calc_unsolve.cpp
+++ 17140_array_calc_unsolve.cpp
@@ -16,7 +16,9 @@ int main()
 {
 	int row, col, k;
 	int map[101][101];
+	int visited[101][101];
 	memset(map, 0, sizeof(map));
+	memset(visited, 0, 0);
 	cin >> row >> col >> k;
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -26,11 +28,11 @@ int main()
 	int cnt = 0;
 	int ans = 0;
 	int max_row = 3;
-	int max_col = 3; 
+	int max_col = 3;
 
 	while (1) {
-		if (map[row][col] != 0) {
-			ans = map[row][col];
+		if (map[row][col] == k) {
+			ans = cnt;
 			break;
 		}
 		else if (cnt > 100) {
@@ -40,17 +42,26 @@ int main()
 
 		if (max_row >= max_col) {	//R연산 -> 오른쪽으로 증가
 			for (int i = 0; i < max_row; i++) {
-				for (int j = 0; j < max_col; j++) {
-
+				v[i].push_back(item((map[i][0]), 1));
+				//visited[i][0] = true;
+				for (int j = 1; j < max_col; j++) {
+					for (int k = 0; k < v[i].size(); k++) {
+						if (v[i][k].num == map[i][j]) {
+							v[i][j].cnt++;
+						}
+						else {
+							v[i].push_back(item((map[i][j]), 1));
+						}
+					}
 				}
 			}
 
 		}
-		else if(max_row < max_col) {	//C연산 -> 아래로 증가
+		else if (max_row < max_col) {	//C연산 -> 아래로 증가
 
 		}
 
-		
+
 
 		cnt++;
 	}
