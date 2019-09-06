297a7a7f44867bda0ba53c4f471475e5f6f7a573
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2019-09-06 11:50:09 +0900

    Update 15683_observe_unsolve.cpp

--- 15683_observe_unsolve.cpp
+++ 15683_observe_unsolve.cpp
@@ -1,101 +1,115 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <algorithm>
+#include <cstring>
 
 using namespace std;
 
-int row, col;
-int map[9][9];
-int temp_map[9][9];
-int visited[9];
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
-int minValue = 9999;
-
 struct pos {
-	int x, y;
-	int value;
-	pos(int a, int b,int c) : y(a), x(b), value(c) {}
+	int y, x, camera_type;
+	pos(int a, int b,int c) : y(a), x(b), camera_type(c) {}
 };
 
-int solve(vector<pos>& tv) {
+struct value {
+	int y, x, dir, camera_type;
+	value(int a, int b, int c, int d) : y(a), x(b), dir(c), camera_type(d) {}
+};
 
-	int ans = 0;
+int cctv_dir1[][4] = { {1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+int cctv_dir2[][4] = { {0,1,0,1},{1,0,1,0}};
+int cctv_dir3[][4] = { {1,1,0,0},{0,1,1,0},{0,0,1,1},{1,0,0,1} };
+int cctv_dir4[][4] = { {1,1,1,0},{0,1,1,1},{1,0,1,1},{1,1,0,1} };
+int cctv_dir5[][4] = { {1,1,1,1} };
+int N, M;
+int map[9][9];
+int dx[] = { 0,1,0,-1 };
+int dy[] = { -1,0,1,0 };
+vector<pos> v;
+
+void copy_map(int origin_map[][9], int copy[][9]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			copy[i][j] = origin_map[i][j];
+		}
+	}
+}
+
+void solve(vector<value>& tv) {
+	int temp_map[9][9];
+	memset(temp_map, 0, sizeof(temp_map));
+	copy_map(map, temp_map);
 
 	for (int i = 0; i < tv.size(); i++) {
-		int c_y = tv[i].y;
-		int c_x = tv[i].x;
-		int c_value = tv[i].value;
-		int cnt1 = -9999;
-		int cnt2 = -9999;
-		int cnt3 = -9999;
-		int cnt4 = -9999;
-		int cnt5 = -9999;
-		if (c_value == 1) {
-			int cnt = 0;
-			for (int j = 0; j < 4; j++) {
-				int n_x = c_x + dx[j];
-				int n_y = c_y + dy[j];
-				while (1) {
-					if (n_y >= row || n_x >= col || n_x < 0 || n_y < 0) {
-						break;
-					}
-					if (map[n_y][n_x] != 6) {
-						n_x += dx[j];
-						n_y += dy[j];
-						cnt++;
-					}
-					else if (map[n_y][n_x] == 6) {
-						break;
-					}
-				}
-				if (cnt1 < cnt) {
-					cnt1 = cnt;
-					memset(temp_map, 0, sizeof(temp_map));
-					int t_x = c_x + dx[j];
-					int t_y = c_y + dy[j];
-					while (1) {
-						if (t_y >= row || t_x >= col || t_x < 0 || t_y < 0) {
-							break;
-						}
-						if (map[t_y][t_x] == 6) {
-							break;
-						}
-						if (map[t_y][t_x] == 0){
-							temp_map[t_y][t_x] = 7;
-						}
-						t_x += dx[j];
-						t_y += dy[j];
-					}
-				}
-			}
-		}
-		else if (c_value == 2) {
-
+		int cx = tv[i].x;
+		int cy = tv[i].y;
+		int c_dir = tv[i].dir;
+		int c_camera_type = tv[i].camera_type;
+		if (c_dir == 1) {
 		}
 	}
 
-	return 0;
+
+	return;
 }
 
+void select_camera_dir(vector<value> &tv, int idx) {
+	if (tv.size() == v.size()) {
+		solve(tv);
+		return;
+	}
+
+	for (int i = idx; i < v.size(); i++) {
+		if (v[i].camera_type == 1) {
+			for (int j = 0; j < 4; j++) {
+				tv.push_back(value(v[i].y, v[i].x, j, v[i].camera_type));
+				select_camera_dir(tv, idx + 1);
+				tv.pop_back();
+			}
+		}
+		else if (v[i].camera_type == 2) {
+			for (int j = 0; j < 2; j++) {
+				tv.push_back(value(v[i].y, v[i].x, j, v[i].camera_type));
+				select_camera_dir(tv, idx + 1);
+				tv.pop_back();
+			}
+		}
+		else if (v[i].camera_type == 3) {
+			for (int j = 0; j < 4; j++) {
+				tv.push_back(value(v[i].y, v[i].x, j, v[i].camera_type));
+				select_camera_dir(tv, idx + 1);
+				tv.pop_back();
+			}
+		}
+		else if (v[i].camera_type == 4) {
+			for (int j = 0; j < 4; j++) {
+				tv.push_back(value(v[i].y, v[i].x, j, v[i].camera_type));
+				select_camera_dir(tv, idx + 1);
+				tv.pop_back();
+			}
+		}
+		else if (v[i].camera_type == 5) {
+			tv.push_back(value(v[i].y, v[i].x, 0, v[i].camera_type));
+			select_camera_dir(tv, idx + 1);
+			tv.pop_back();
+		}
+	}
+}
 
 int main() {
 
-	cin >> row >> col;
-	vector<pos> v,temp_v;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			cin >> map[i][j];
-			temp_map[i][j] = map[i][j];
-			if (map[i][j] != 0)
-				v.push_back(pos(i, j,map[i][j]));
+			if (map[i][j] >= 1 && map[i][j] <= 5) {
+				v.push_back(pos(i, j, map[i][j]));
+			}
 		}
 	}
 
-	solve(v);
-
-
+	vector<value> tv;
+	select_camera_dir(tv, 0);
 
 	return 0;
 }
