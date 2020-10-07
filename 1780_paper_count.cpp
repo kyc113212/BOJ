cd948a57eb50f221b124ebb465ef17bec0555d37
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2020-10-08 01:37:28 +0900

    Update 1780_paper_count.cpp

--- 1780_paper_count.cpp
+++ 1780_paper_count.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int map[2200][2200];
 int check_1[2200][2200];
+int visited[2200][2200];
 int N;
 
 int dx[] = { -1,1,0,0 };
@@ -30,7 +31,7 @@ int main() {
 
 	for (int i = 0; i < N; i += 3) {
 		for (int j = 0; j < N; j += 3) {
-			
+
 			int pivot_value = map[i][j];
 			bool flag = false;
 			for (int m = i; i < i + 3; m++) {
@@ -44,7 +45,7 @@ int main() {
 			if (flag == false) {
 				for (int m = i; i < i + 3; m++) {
 					for (int n = j; j < j + 3; n++) {
-						check_1[m][n] = 1;	//1개의 값
+						check_1[m][n] = 1;	//1이면 한개인 경우
 					}
 				}
 			}
@@ -56,14 +57,59 @@ int main() {
 		int prev = 0;
 		int j = 0;
 		for (j = 0; j < N; j += 3) {
-			if (map[i][prev] != map[i][j])
-				break;
+			if (check_1[i][j] == 1)
+				continue;
+			if (check_1[i][j] == 0) {
+				int max_row = 0;
+				for (int m = i; m < N; m++) {
+					if (check_1[m][j] == 1 || map[i][j] != map[m][j]) {
+						max_row = m - 1;
+						break;
+					}
+				}
+
+				int max_column = 0;
+				for (int m = j; m < N; m++) {
+					if (check_1[i][m] == 1 || map[i][j] != map[i][m]) {
+						max_column = m - 1;
+						break;
+					}
+				}
+
+				if (max_row - i < 8 || max_column - j < 8) {	// 3X3인 종이들
+					for (int m = i; m <= max_row; m++) {
+						for (int n = j; n <= max_column; n++) {
+							check_1[m][n] = 2;	//2이면 숫자는 같지만 3X3인 경우
+						}
+					}
+				}
+				else {
+					//범위 내 값이 모두 같은값인지 확인
+					bool flag = false;
+					for (int m = i; m <= max_row; m++) {
+						for (int n = j; j <= max_column; n++) {
+							if (map[i][j] != map[m][n]) {
+								flag = true;
+								break;
+							}
+						}
+					}
+					int range = max(max_row - i, max_column - j);
+					if (flag == false) {
+						for (int m = i; m <= range; m++) {
+							for (int n = j; j <= range; n++) {
+								check_1[m][n] = 3;	//3이상이면 9x9이상인 종이
+							}
+						}
+					}
+				}
+
+			}
 		}
-		if()
 	}
 
 
-	
+
 
 
 	return 0;
