e6b1deebe954b7140f41cffe99a923b0f88ed6bb
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2022-07-11 15:08:56 +0900

    Create 1676_count_of_factorial_s_zero.cpp

---
+++ NMP/Math/1676_count_of_factorial_s_zero.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <deque>
+#include <cstring>
+
+#define MAX_V 987654321
+#define MOD 100000009
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N/5 + N/25 + N/125 << '\n';
+
+
+	return 0;
+}
