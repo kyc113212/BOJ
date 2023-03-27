eace844de2f8dfc7204c78894250c157fba3e7f1
kyc113212 <35021495+kyc113212@users.noreply.github.com>
2023-03-27 14:53:56 +0900

    Create 14430_ResourceDigging.py

---
+++ DynamicProgramming/14430_ResourceDigging.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+from collections import deque
+from heapq import heappush, heappop
+import math
+
+#sys.setrecursionlimit(10**9)
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+DP = [[0] * (M+1) for _ in range(N+1)]
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        DP[i][j] = max(DP[i-1][j] + board[i-1][j-1], DP[i][j-1] + board[i-1][j-1])
+print(DP[N][M])
